41e5ecd2973391287745086534227c55218d38fb
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-17 19:23:05 +0530

    Change

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello world")
+print(1)
+print(2+3)
+print("2+3")
+print("Hello i am Arnav Tyagi.", 1 , 'I study in grade 7th')
+print("""my hobbies are
+       playing badminton
+        Cricket
+        Music
+        """)

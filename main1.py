5362aa651b2c460c9de83a2f2a2b0add45a457fb
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-19 19:10:42 +0530

    change

--- main1.py
+++ main1.py
@@ -8,3 +8,8 @@ print("Hello Python \n")
 print("hello",5)
 
 print("welcome to ", end= "*")
+
+name = input("Hi, What's your name?")
+print("Hi", name, "Welcome.")
+import keyword
+print(keyword.kwlist)

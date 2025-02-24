ba3b223923c333eabc9fca486381e75342241bfa
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-24 19:17:17 +0530

    changes

---
+++ main4.py
@@ -0,0 +1,25 @@
+name = "Arnav"
+age = 12
+student = True
+weight = 29.5
+
+print("Name:", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("is student :", student)
+print("Data Type of student? is", type(student))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+
+print("\n After Type Casting .... ")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))

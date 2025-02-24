372ac902fd1cdc659b313a3dd680c8e9d79e8f6b
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-24 19:06:41 +0530

    change

---
+++ main2.py
@@ -0,0 +1,28 @@
+import random
+
+def get_random_greeting():
+    greetings = [
+        "Welcome to Python!", 
+        "Hello, Pythonista!", 
+        "Ahoy! Ready for Python?", 
+        "Greetings!", 
+        "Python welcomes you with open arms!"
+    ]
+    return random.choice(greetings)
+
+def main():
+    print(get_random_greeting())
+    name = input("Enter your name: ")
+    city = input("Enter your city: ")
+    
+    twist = random.choice([
+        f"Hi {name}, you live in {city}. That's awesome!",
+        f"Oh wow, {name} from {city}! Sounds like a great place!",
+        f"{name}, {city} is lucky to have you!",
+        f"Ever thought of coding in {city}, {name}? Bet it's a cool vibe!"
+    ])
+    
+    print(twist)
+
+if __name__ == "__main__":
+    main()
---
+++ main3.py
@@ -0,0 +1,14 @@
+a = 5
+print("Type of A:" ,type(a) )
+
+a = 5.1
+print("Type of A:" ,type(a) )
+
+a = True
+print("Type of A:" ,type(a) )
+
+a = False
+print("Type of A:" ,type(a) )
+
+a = "Hello"
+print("Type of A:" ,type(a) )

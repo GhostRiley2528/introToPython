aa367b8889ca12d9572884492ab878bc6b72d174
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-26 19:20:53 +0530

    change

---
+++ main8.py
@@ -0,0 +1,12 @@
+# Input marks for 5 subjects
+s1 = float(input("Enter marks for subject 1: "))
+s2 = float(input("Enter marks for subject 2: "))
+s3 = float(input("Enter marks for subject 3: "))
+s4 = float(input("Enter marks for subject 4: "))
+s5 = float(input("Enter marks for subject 5: "))
+
+# Calculate percentage
+percentage = (s1 + s2 + s3 + s4 + s5) / 5
+
+# Print result
+print("Percentage:", percentage, "%")

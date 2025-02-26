7efd3d692232b8e4925f7f972bb541dcd0080924
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-02-26 19:06:35 +0530

    change

---
+++ main6.py
@@ -0,0 +1,9 @@
+tree1 = int(input("What is the length of the first tree?"))
+tree2 = int(input("What is the length of the second tree?"))
+tree3 = int(input("What is the length of the third tree?"))
+tree4 = int(input("What is the length of the fourth tree?"))
+tree5 = int(input("What is the length of the fifth tree?"))
+
+sum= tree1+tree2+tree3+tree4+tree5
+print("The total length of the trees is:" , sum)
+print("The average height of the length of the trees is: " , sum/5)

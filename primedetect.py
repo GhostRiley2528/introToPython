74b564eca7545ee5e38f217381465ee65965b895
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-04-10 11:37:00 +0530

    change

--- primedetect.py
+++ primedetect.py
@@ -1,6 +1,6 @@
 up = int(input("Enter the maximum range: "))
 dn = int(input("Enter the minimum range: "))
-print("The prime numbers between ", up , " and ", dn, " are: ", i)
+print("The prime numbers between ", up , " and ", dn, " are: ")
 
 for i in range (dn, up, 1):
     if i > 1:
@@ -8,4 +8,4 @@ for i in range (dn, up, 1):
             if (i % num) == 0:
                break
         else:
-            print("The prime numbers between ", up , " and ", dn, " are: ", i)
+            print(i, end=", ")

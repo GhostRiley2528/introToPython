577d4c6e2ce2cd9252d776bd20eb81f04ef126db
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-04-13 13:01:00 +0530

    change

--- pattern.py
+++ pattern.py
@@ -1,6 +1,17 @@
-x = int(input("enter number of rows: "))
+'''x = int(input("enter number of rows: "))
 x = x+1
 for i in range(1, x, 1):
     for n in range(1, i +1):
         print(n , end=" ")
+    print()'''
+
+l = int(input("enter number of rows: "))
+
+
+for f in range(1, l, 1):
+    for space in range(l - f):
+        print(" ", end = " " )
+    num = 1
+    for j in range(2* f - 1):
+        print(num, end=" ")
     print()

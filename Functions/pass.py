14e2c33353b2df4f7ef34952574221818e4cdee8
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-04-27 12:32:24 +0530

    change

--- Functions/pass.py
+++ Functions/pass.py
@@ -2,12 +2,16 @@ def twist_fizz_buzz(start, end):
     for num in range(start, end+1):
         if num % 20 == 0:
             print("twist")
-            break
         elif num % 15 == 0:
             pass
         elif num % 5 == 0:
             print("fizz")
-            break
         elif num % 3 == 0:
             print("buzz")
-            break
+        else:
+            print(num)
+print("Enter the start number: ")
+start = int(input())
+print("Enter the end number: ")
+end = int(input())
+twist_fizz_buzz(start, end)

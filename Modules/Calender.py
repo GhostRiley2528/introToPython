9abd373cbfefc1a05d2ebc015b02aef05f21f1d5
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-05-04 13:12:07 +0530

    chnages

---
+++ Modules/Calender.py
@@ -0,0 +1,23 @@
+import sys
+from time import sleep
+import calendar
+
+def slow_type(text, delay=0.1):
+    for char in text:
+        sleep(delay)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    print()  
+
+def monthy():
+    slow_type("Welcome to the Monthly Calendar Viewer!\n", 0.1)
+    year = int(input("Enter year (e.g., 2025): "))
+    month = int(input("Enter month (1-12)  [NUMBER FORMAT ONLY]: "))
+    slow_type(calendar.month(year, month) , 0.1)
+    x = input("Want to try again? (y/n) ")
+    if x.lower() == "y":
+        monthy()
+    else:
+        slow_type("Shutting down...", 0.3)
+
+monthy()

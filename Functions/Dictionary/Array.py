120353923c1e7da06aea29ef9ad04793d6eb506e
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-06-09 19:02:48 +0530

    change

---
+++ Functions/Dictionary/Array.py
@@ -0,0 +1,20 @@
+set = {1,2,3}
+print(set)
+
+my_set = {1,2,3,2,1,3}
+print(my_set)
+
+set0 = {1,2,3,0.4,"hello"}
+print(set0)
+
+set1 = set([1,2,3,4,5])
+print(set1)
+
+num_set = {1,2,3,4,5}
+print(num_set)
+num_set.pop()
+print(num_set)
+
+set2 = num_set
+set2.add(6)
+print(set2)

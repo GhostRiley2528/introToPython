39c2bef3906eda06fb2800986778931598a00210
GhostRiley2528 <mailarnavtyagi@gmail.com>
2025-07-07 19:20:28 +0530

    changes

--- OOP/Employee.py
+++ OOP/Employee.py
@@ -1,16 +1,19 @@
+import time
 class Employee:
     def __init__(self):
         print("Employee created ")
-
+        time.sleep(2) 
     def __del__(self):
         print("Destrcutor called  ")
-
+        time.sleep(2)
 def create_and_delete_employee():
     print("Creating an employee object...")
+    time.sleep(2)
     emp = Employee()  
     print("Employee object created...")
+    time.sleep(2)
     print("Function ending...")
     del emp         
 
-
+time.sleep(2)
 create_and_delete_employee()
